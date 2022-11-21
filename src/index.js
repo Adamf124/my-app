27a07997473cb2631e5648970119bef43f203024
Adam <adam.ferguson124@gmail.com>
2022-11-21 18:06:50 -0500

    display the value of state when clicked

--- src/index.js
+++ src/index.js
@@ -12,8 +12,10 @@ class Square extends React.Component {
   }
   render() {
     return (
-      <button className="square" onClick={ ()=>{console.log('I HAVE BEEN CLICKED!');} }>
-        {this.props.value}
+      <button 
+        className="square" 
+        onClick={ ()=>{this.setState({value: 'X'});} }>
+        {this.state.value}
       </button>
     );
   }
