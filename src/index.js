eb15ce8c3be8c32b90e93f6805e250d6322a1135
Adam <adam.ferguson124@gmail.com>
2022-12-21 15:39:28 -0500

    sheesh

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ class Game extends React.Component {
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
-    //uh oh
+    //almost broke it lol
     //Note: Call .slice() to create a copy of the squares array to modify instead of modifying the existing array.
     squares[i] = this.state.xIsNext ? 'X' : 'O'
     this.setState({
